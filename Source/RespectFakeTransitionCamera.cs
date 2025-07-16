6fe07689d6067d77bc94e280ecda401b8316c1c3
kyfex-uwu <kyfexuwu@gmail.com>
2025-07-15 23:34:10 -0600

    - fixed some rftc collision issues

--- Source/RespectFakeTransitionCamera.cs
+++ Source/RespectFakeTransitionCamera.cs
@@ -14,12 +14,19 @@ public class RespectFakeTransitionCamera : Trigger {
         if (FakeRoomTransition.lockCam && self.CollideAll<RespectFakeTransitionCamera>().Count != 0) {
             var cam = self.SceneAs<Level>().Camera;
             foreach (var transition in self.Scene.Tracker.GetEntities<FakeRoomTransition>()) {
-                if ((transition as FakeRoomTransition).isHorizontal) {
-                    if (self.Center.Y > transition.Y) cam.Y = Math.Max(cam.Y, transition.Y);
-                    else cam.Y = Math.Min(cam.Y, transition.Y - cam.Viewport.Height);
+                var frt = transition as FakeRoomTransition;
+                if (frt.isHorizontal) {
+                    if (cam.Y < frt.Y && cam.Y + cam.Viewport.Height > frt.Y &&
+                        cam.X - frt.size/8f < frt.X && cam.X + cam.Viewport.Width > frt.X) {
+                        if (self.Center.Y > transition.Y) cam.Y = Math.Max(cam.Y, transition.Y);
+                        else cam.Y = Math.Min(cam.Y, transition.Y - cam.Viewport.Height);
+                    }
                 } else {
-                    if (self.Center.X > transition.X) cam.X = Math.Max(cam.X, transition.X);
-                    else cam.X = Math.Min(cam.X, transition.X - cam.Viewport.Width);
+                    if (cam.X < frt.X && cam.X + cam.Viewport.Width > frt.X &&
+                        cam.Y - frt.size/8f < frt.Y && cam.Y + cam.Viewport.Height > frt.Y) {
+                        if (self.Center.X > transition.X) cam.X = Math.Max(cam.X, transition.X);
+                        else cam.X = Math.Min(cam.X, transition.X - cam.Viewport.Width);
+                    }
                 }
             }
         }
