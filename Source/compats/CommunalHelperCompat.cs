cae0ea120c14f8a28f3f47f4fe73c0ee75a28d00
kyfex-uwu <kyfexuwu@gmail.com>
2025-12-15 00:33:38 -0700

    - fix inf length bug - bumpver

--- Source/compats/CommunalHelperCompat.cs
+++ Source/compats/CommunalHelperCompat.cs
@@ -18,6 +18,7 @@ public class CommunalHelperCompat {
             changeLength));
         hooks.Add(new Hook(typeof(SegmentBooster).GetMethod("RedDashUpdateBefore", BindingFlags.Instance | BindingFlags.NonPublic),
             changeLength2));
+        On.Celeste.Level.Update += cleanInfLength;
         On.Celeste.Booster.PlayerReleased += oneTime;
         CloneBooster.Load();
         CloneDreamBooster.LoadHooks();
@@ -36,34 +37,54 @@ public class CommunalHelperCompat {
         }
     }
 
+    private static void cleanInfLength(On.Celeste.Level.orig_Update orig, Level self) {
+        orig(self);
+        infLength.RemoveWhere(entityRef => !entityRef.TryGetTarget(out var _));
+    }
     private static int? changeLength(Func<DreamBoosterSegment, Player, int?> orig, DreamBoosterSegment self, Player player) {
         var oldLength = self.Length;
-        self.Length = float.PositiveInfinity;
+        foreach (var entityRef in infLength) {
+            if (entityRef.TryGetTarget(out var booster) && booster == self) {
+                self.Length = float.PositiveInfinity;
+                break;
+            }
+        }
         var toReturn = orig(self, player);
         self.Length = oldLength;
         return toReturn;
     }
     private static int? changeLength2(Func<SegmentBooster, Player, int?> orig, SegmentBooster self, Player player) {
         var oldLength = self.Length;
-        self.Length = float.PositiveInfinity;
+        foreach (var entityRef in infLength) {
+            if (entityRef.TryGetTarget(out var booster) && booster == self) {
+                self.Length = float.PositiveInfinity;
+                break;
+            }
+        }
         var toReturn = orig(self, player);
         self.Length = oldLength;
         return toReturn;
     }
 
+    private static HashSet<WeakReference<Entity>> infLength = new();
     // private static FieldInfo spiralBoosterClockwise = typeof(SpiralBooster).GetField("clockwise", BindingFlags.Instance | BindingFlags.NonPublic);
     private static FieldInfo curvedBoosterSpeed = typeof(CurvedBooster).GetField("endingSpeed", BindingFlags.Instance | BindingFlags.NonPublic);
     public static void DerailBooster(Entity booster, Vector2 position, Player player) {
-        if (booster is DreamBoosterCurve dream) {
+        if (booster is DreamBoosterCurve dreamCurved) {
             booster.Scene.Add(new CloneDreamBooster(position));
             player.Speed.Normalize();
-            player.Speed *= dream.EndingSpeed.Length();
+            player.Speed *= dreamCurved.EndingSpeed.Length();
         }
         if (booster is CurvedBooster curved) {
             booster.Scene.Add(new CloneBooster(position, true));
             player.Speed.Normalize();
             player.Speed *= ((Vector2)curvedBoosterSpeed.GetValue(curved)).Length();
         }
+
+        if (booster is DreamBoosterSegment || booster is SegmentBooster) {
+            infLength.Add(new WeakReference<Entity>(booster));
+        }
+
         if (booster is SpiralBooster spiral) {
             // booster.Scene.Add(new CloneBooster(position, true, 
             //     CommunalHelperGFX.SpriteBank.Create((bool)spiralBoosterClockwise.GetValue(spiral) ? "clockwiseSpiralBooster" : "counterclockwiseSpiralBooster")));
